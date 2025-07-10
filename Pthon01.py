68396e0256fc744b4d66d7b93d241accdb3d9a99
Manuzinha1503 <emanuellibritz@gmail.com>
2025-07-10 08:49:43 -0300

    Add files via upload

---
+++ Pthon01.py
@@ -0,0 +1,14 @@
+print("Variaveis")
+nome = "Emanuelli"
+print("Bom dia "+ nome)
+idade = 15
+print(idade)
+salario_minimo = 1450.99
+print(salario_minimo)
+ativo = True
+print(ativo)
+print(type(nome))
+
+print("== Cadastro da Escola ==")
+nome = imput ("digite o nome: ")
+print("bem vindo(a) " +nome)
